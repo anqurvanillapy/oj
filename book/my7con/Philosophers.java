29f0385d6af138188f9968e3baaa9c7380f5780a
AnqurVanillapy <anqurvanillapy@gmail.com>
2016-12-15 00:56:00 +0800

    added dining philosophers problem todos

--- book/my7con/Philosophers.java
+++ book/my7con/Philosophers.java
@@ -5,6 +5,19 @@
  *  > From ch2d1
  *
  *  Dining philosophers problem.
+ *
+ *  XXX TODO:
+ *  XXX
+ *  XXX There are about 4 solutions to this problem, which need to be discussed
+ *  XXX and implemented here.
+ *  XXX
+ *  XXX 1.  At most 4 philosophers are required to pick up the left chopstick
+ *  XXX     simultaneously.
+ *  XXX 2.  Use AND semaphore.
+ *  XXX 3.  Odd-numbered philosophers pick up the left chopstick first, and
+ *  XXX     even-numbered thinkers grab the right one first.
+ *  XXX 4.  Number each chopstick. The first picked chopstick should be greater
+ *  XXX     (less) than the second one.
  */
  
  class Philosopher extends Thread {
