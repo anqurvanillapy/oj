54af892f44448ddb341ce5bb8078cd7242c73cd7
AnqurVanillapy <anqurvanillapy@gmail.com>
2017-01-21 03:08:40 +0800

    poor AC climb stairs

--- leetcode/climb-stairs/climb_stairs.c
+++ leetcode/climb-stairs/climb_stairs.c
@@ -14,13 +14,17 @@
 #include <stdio.h>
 
 int climbStairs(int n);
-int step1(int n);
-int step2(int n);
+
+// int step1(int n);
+// int step2(int n);
+
+double combinate(double n, double k);
+double permute(double n, double k);
 
 int
 main(int argc, const char *argv[])
 {
-    int n = 4;
+    int n = 27;
     printf("%d\n", climbStairs(n));
 
     return 0;
@@ -51,5 +55,39 @@ main(int argc, const char *argv[])
 //     if (n < 2) return 0;
 //     return step1(n - 2) + step2(n - 2);
 // }
+
+/**
+ *  AC (poorly): Using the formula of combinations and permutations
+ */
+
+int
+climbStairs(int n)
+{
+    double count = 1;
+    double dual, mono;
+
+    for (dual = 1; dual <= n / 2; ++dual) {
+        mono = n - dual * 2;
+        count += (dual < mono) ? combinate(dual + mono, mono) : combinate(dual + mono, dual);
+    }
+
+    return (int)count;
+}
+
+double
+combinate(double n, double k)
+{
+    if (k == 0 || n == k) return 1;
+    if (k > n / 2) k = n - k;
+    return permute(n, k) / permute(k, k);
+}
+
+double
+permute(double n, double k)
+{
+    double i, j, ret = 1;
+    for (i = n, j = 0; j < k; --i, ++j) ret *= i;
+    return ret;
+}
 
 /* === EOS === */
