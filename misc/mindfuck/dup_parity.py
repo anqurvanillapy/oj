2a2f45b7bfd5f024f47f3c79c5ec661b6e00e1d6
AnqurVanillapy <anqurvanillapy@gmail.com>
2017-04-03 14:48:18 +0800

    fixed dup_parity: renamed vars

--- misc/mindfuck/dup_parity.py
+++ misc/mindfuck/dup_parity.py
@@ -1,10 +1,10 @@
 """\
-    Duplicate even or odd numbers in a list,achieved by a pretty Lisp-like
+    Duplicate even or odd numbers in a list, achieved by a pretty Lisp-like
     Python code.
 """
 
 print(
-    (lambda fn0: lambda a0, b0: fn0(fn0, a0, b0)) \
+    (lambda fn: lambda a, b: fn(fn, a, b)) \
         (lambda f, ls, dup: [] if len(ls) == 0 else \
             (lambda y, nls, d: \
                 [nls[0]] + [nls[0]] + y(y, nls[1:], not d) if d \
