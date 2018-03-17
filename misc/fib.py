7a18471a2384ac1d085bd6f5cec370b0d07db593
AnqurVanillapy <anqurvanillapy@gmail.com>
2018-03-18 00:44:41 +0800

    updated fib.py: functools.lru_cache

--- misc/fib.py
+++ misc/fib.py
@@ -7,6 +7,7 @@
     * Iterative, but a generator
 """
 
+import functools
 from benchmark import calltime
 
 
@@ -24,15 +25,25 @@ kvs = {}
 def rfib1(n):
     """Imporved recursive function"""
     if n > 2:
-        try: return kvs[n]
-        except: pass
+        try:
+            return kvs[n]
+
+        except:
+            pass
         ret = rfib1(n - 1) + rfib1(n - 2)
         kvs[n] = ret
         return ret
 
-        # Interesting that it doesn't work.
-        # return kvs.setdefault(n, rfib1(n - 1) + rfib1(n - 2))
+    # Interesting that it doesn't work.
+    # return kvs.setdefault(n, rfib1(n - 1) + rfib1(n - 2))
     return 1
+
+
+@calltime
+@functools.lru_cache(maxsize=None)
+def rfib2(n):
+    """With functools.lru_cache"""
+    return rfib2(n - 1) + rfib2(n - 2) if n > 2 else 1
 
 
 @calltime
@@ -40,8 +51,10 @@ def ifib(n):
     """Iterative function"""
     a, b = 0, 1
     if n > 1:
-        for i in range(n): a, b = b, a + b
+        for i in range(n):
+            a, b = b, a + b
         return a
+
     return 1
 
 
@@ -51,12 +64,17 @@ def gen_ifib(n):
     while True:
         a, b, i = b, a + b, i + 1
         yield a
-        if i >= n: break
+
+        if i >= n:
+            break
 
 
 # It won't print anything if not converted into a list.
-list(map(lambda x: print('v={}, cnt={}, ms={}'.format(x(30), x.cnt, x.ms)),
-         [rfib, rfib1, ifib]))
-
+list(
+    map(
+        lambda x: print('v={}, cnt={}, ms={}'.format(x(30), x.cnt, x.ms)),
+        [rfib, rfib1, rfib2, ifib],
+    )
+)
 # Mechanism of a generator is different.
 print([i for i in gen_ifib(7)])
